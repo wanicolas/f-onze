f1a0b2ad04531224471654f24b24baf199aa86f5
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-05-19 16:17:04 +0200

    fix: remove unnecessary span tags for improved readability in a-propos.vue

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -5,17 +5,16 @@
 			<span class="font-accent text-accent">audiovisuelle</span>
 		</h1>
 		<p class="mb-20 text-lg md:text-xl">
-			Chez F/ONZE, chaque projet devient une
-			<span class="font-accent">aventure</span>, chaque création une
-			<span class="font-accent">exploration</span>. Grâce à notre expertise en
-			communication et production, nous traversons les univers du digital, du
-			sport et de l'humain, en capturant l'essence de chaque histoire. Ici, on
-			ne se contente pas de produire, on façonne des expériences uniques qui
-			résonnent profondément. Notre objectif va au-delà de la simple production
-			: on cherche à éveiller l’imagination et à enrichir les perspectives.
-			F/ONZE s’engage à repousser les limites de la création traditionnelle, en
-			invitant chacun à plonger dans des mondes où innovation technologique et
-			expression artistique se rencontrent pour susciter émotions et réflexions.
+			Chez F/ONZE, chaque projet devient une aventure, chaque création une
+			exploration. Grâce à notre expertise en communication et production, nous
+			traversons les univers du digital, du sport et de l'humain, en capturant
+			l'essence de chaque histoire. Ici, on ne se contente pas de produire, on
+			façonne des expériences uniques qui résonnent profondément. Notre objectif
+			va au-delà de la simple production : on cherche à éveiller l’imagination
+			et à enrichir les perspectives. F/ONZE s’engage à repousser les limites de
+			la création traditionnelle, en invitant chacun à plonger dans des mondes
+			où innovation technologique et expression artistique se rencontrent pour
+			susciter émotions et réflexions.
 		</p>
 
 		<div class="mb-8 flex h-fit w-full items-center gap-6">
